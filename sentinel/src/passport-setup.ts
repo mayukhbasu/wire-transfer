59b712c21111021b90e48126007ec9526dc403d4
Mayukh Basu <roxy.firebird@gmail.com>
2023-12-05 17:39:57 +0530

    refactored code for passport

--- sentinel/src/passport-setup.ts
+++ sentinel/src/passport-setup.ts
@@ -1,10 +1,11 @@
+// Assuming this is in a file named 'passport-setup.ts'
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { User, IUser } from './models/User';
-
+import logger from './logger'; // import the Winston logger
 
 interface User {
-  id: string
+  id: string;
 }
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
@@ -19,27 +20,31 @@ passport.use(new GoogleStrategy({
     let user = await User.findOne({ googleId: profile.id });
     if (!user) {
       user = await User.create({ googleId: profile.id, displayName: profile.displayName });
+      logger.info(`New user created: ${user.googleId}`);
+    } else {
+      logger.info(`User found: ${user.googleId}`);
     }
     done(null, user as User);
   } catch (error) {
+    logger.error(`Error in Google Strategy: ${error}`);
     done(error as Error);
   }
 }));
 
 passport.serializeUser((user: User, done) => {
   done(null, (user as any).googleId); // or whatever unique identifier you have for the user
+  logger.info(`Serializing user: ${user.id}`);
 });
 
 passport.deserializeUser(async (googleId: string, done) => {
   try {
     const user = await User.findOne({ googleId: googleId }) as User | null;
     done(null, user);
+    logger.info(`Deserializing user: ${googleId}`);
   } catch (error) {
+    logger.error(`Error in deserializing user: ${error}`);
     done(error as Error);
   }
 });
 
-
 export default passport;
-
-
--- sentinel/src/routes/auth.ts
+++ sentinel/src/routes/auth.ts
@@ -1,15 +1,25 @@
-import { Router } from 'express';
+import { Errback, NextFunction, Request, Response, Router } from 'express';
 import passport from '../passport-setup'; // Adjust the import path as necessary
+import logger from '../logger'; // import the Winston logger
 
 const router = Router();
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }), (req, res, next) => {
+  logger.info('Redirecting to Google for authentication');
+  next(); // proceed to the next middleware (passport.authenticate in this case)
+});
 
 router.get('/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
-  (req, res) => {
+  (req: Request, res: Response) => {
     // Successful authentication, redirect home.
+    logger.info('Google authentication successful, redirecting to home');
     res.redirect('/');
+  },
+  (err: Errback, req: Request, res: Response, next: NextFunction) => {
+    // Error handler
+    logger.error(`Google authentication error: ${err}`);
+    res.redirect('/login');
   }
 );
 
