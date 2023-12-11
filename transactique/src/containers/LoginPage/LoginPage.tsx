a92543041c2e1831bb68897ce4076f67e820b051
Mayukh Basu <roxy.firebird@gmail.com>
2023-12-11 18:42:11 +0530

    created basic login page

---
+++ transactique/src/containers/LoginPage/LoginPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './LoginPage.css';
+
+const LoginPage = () => {
+
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:3000/auth/google';
+  }
+  return (
+    <div className="loginContainer">
+      <h1 className="title">Welcome to Wires</h1>
+      <button className="googleButton" onClick={handleLogin}>
+        Sign in with Google
+      </button>
+    </div>
+  );
+};
+
+export default LoginPage;
