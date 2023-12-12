d498b76e329e235b12e3f571df224897bbab32c4
Mayukh Basu <roxy.firebird@gmail.com>
2023-12-12 18:04:55 +0530

    create other account flow is completed

--- sentinel/src/routes/user-account.ts
+++ sentinel/src/routes/user-account.ts
@@ -7,6 +7,7 @@ const userController = new UserController();
 
 router.post('/createCustomer',isAuthenticated, (req: Request, res: Response) => userController.createUserAccount(req, res))
 router.get('/createCustomer', isAuthenticated, (req: Request, res: Response) => userController.getUserAccounts(req, res));
+router.post('/createOtherAccounts', isAuthenticated, (req: Request, res: Response) => userController.createOtherAccounts(req, res));
 router.post('/updatebalance', isAuthenticated, (req: Request, res: Response) => userController.updatebalance(req, res));
 router.get('/getTotalBalance', isAuthenticated, (req: Request, res: Response) => userController.getTotalbalance(req, res));
 
