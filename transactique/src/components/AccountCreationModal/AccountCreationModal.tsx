263f78ab8e0589d4eca188a9a67487788f29a207
Mayukh Basu <roxy.firebird@gmail.com>
2024-01-20 06:09:37 +0530

    added account creation modal

--- transactique/src/components/AccountCreationModal/AccountCreationModal.tsx
+++ transactique/src/components/AccountCreationModal/AccountCreationModal.tsx
@@ -1,22 +1,55 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import styles from  './AccountCreationModal.module.css';
 
 const AccountCreationModal = () => {
+  const [accountType, setAccountType] = useState('Savings');
+
+  const handleAccountTypeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setAccountType(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    console.log(`Account Type Selected: ${accountType}`);
+  };
   return (
     <div className={styles.modalBackground} id="modalBackground">
   <div className={styles.modal}>
     <button className={styles.closeModal}>×</button>
     <h2>Add Account</h2>
-    <form id="account-form">
+    <form id="account-form" onSubmit={handleSubmit}>
       <label htmlFor="savings-account">
-        <input type="radio" id="savings-account" name="account-type" value="Savings"/>
+        <input
+          type="radio"
+          id="savings-account"
+          name="account-type"
+          value="Savings"
+          checked={accountType === 'Savings'}
+          onChange={handleAccountTypeChange}
+        />
         Savings Account
       </label>
       <label htmlFor="current-account">
-        <input type="radio" id="current-account" name="account-type" value="Current"/>
+        <input
+          type="radio"
+          id="current-account"
+          name="account-type"
+          value="Current"
+          checked={accountType === 'Current'}
+          onChange={handleAccountTypeChange}
+        />
         Current Account
       </label>
       <label htmlFor="investment-account">
-        <input type="radio" id="investment-account" name="account-type" value="Investment"/>
+        <input
+          type="radio"
+          id="investment-account"
+          name="account-type"
+          value="Investment"
+          checked={accountType === 'Investment'}
+          onChange={handleAccountTypeChange}
+        />
         Investment Account
       </label>
       <button type="submit">Add Account</button>
