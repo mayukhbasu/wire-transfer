c13d92868823fd0e4dc6ffb965906a60e44a63b9
Mayukh Basu <roxy.firebird@gmail.com>
2024-01-31 06:05:54 +0530

    basic design of fund transfer

--- transactique/src/containers/FundTransfer/FundTransfer.tsx
+++ transactique/src/containers/FundTransfer/FundTransfer.tsx
@@ -3,8 +3,12 @@ import './FundTransfer.css';
 const FundTransfer = () => {
   return (
     <div data-testid="fund-transfer" className='container'>
+      
       <div className="centered-box">
-        
+        <div className='account-transfers'>
+          <button>Transfer Own Account</button>
+          <button>Transfer Other Account</button>
+        </div>
       </div>
     </div>
   );
